0e25ed7f4af5afedde1f4883f579df829e213743
Hidden Gem - Bidyut Roy <74731201+BidyutRoy2@users.noreply.github.com>
2024-07-29 15:53:50 +0600

    proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -218,6 +218,8 @@ sed -i 's/0x[0-9a-fA-F]*,\?\s*//g' .env
 echo
 print_blue "PRIVATE_KEY has been removed from .env."
 echo
+echo rm .env 
+echo
 print_blue "Pushing these files to your github Repo link"
 git add . && git commit -m "Initial commit" && git push origin main
 echo
